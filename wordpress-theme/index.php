841d55cec9c907705b2df92aaa2b021f8aa578ff
AitorArcas890 <aar890@educa.madrid.org>
2026-04-22 08:34:41 +0200

    Primer HTML

---
+++ wordpress-theme/index.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Template Name: WebFusion Home
+ */
+?>
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>WebFusion Digital S.L.</title>
+</head>
+<body>
+<main>
+	<h1>WebFusion Digital</h1>
+</main>
+<footer>
+    <p>&copy; <?php echo date('Y'); ?> WebFusion Digital S.L.</p>
+</footer>
+</body>
+</html>
