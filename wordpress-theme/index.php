27ee65244ccf4af75e25dd659ac6658080305e4c
Aitor <aitorarcasaitorarcas@hotmail.com>
2026-04-22 08:04:24 +0200

    Añadir primer HTML

---
+++ wordpress-theme/index.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Template Name: WebFusion Home
+ */
+?>
+<!DOCTYPE html>
+<html lang="es">
+</head>
+<body>
+<h1>WebFusion Digital</h1>
+<footer>
+    <p>&copy; <?php echo date('Y'); ?> WebFusion Digital S.L.</p>
+</footer>
+</body>
+</html>
